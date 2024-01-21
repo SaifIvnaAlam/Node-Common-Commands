15dd380bfa74960a65d7be97868c726500d35c22
saif ivna alam <alam.saifivna@gmail.com>
2024-01-21 17:32:02 +0600

    update: inquirer package added

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,30 @@
 #!/usr/bin/env node
-
-const listOfcommands = {
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const listOfFluttercommands = {
   flutterbuildScript:
     "flutter packages pub run build_runner watch --delete-conflicting-outputs",
 };
 
-console.log(listOfcommands["flutterbuildScript"]);
+async function run() {
+  const choices = inquirer.prompt([
+    {
+      type: "list",
+      name: "list",
+      message: "Choose a Language",
+      choices: ["flutter", "JavaScript"],
+    },
+  ]);
+  return choices;
+}
+
+run().then((choices) => {
+  console.log(chalk.blue(choices));
+  if (choices.list == "flutter") {
+    for (var key in listOfFluttercommands) {
+      console.log(key + " : " + listOfFluttercommands[key]);
+    }
+  } else {
+    console.log("No commands found for " + choices.list);
+  }
+});
